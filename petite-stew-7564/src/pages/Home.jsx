a26b06e67e89d1d207c3cfbb4a3793f5deb0318f
abhayfaldu <abhayfaldu1922@gmail.com>
2022-09-29 23:41:15 +0530

    working on homepage header

--- petite-stew-7564/src/pages/Home.jsx
+++ petite-stew-7564/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import HomeHeader from '../components/HomeHeader'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <HomeHeader />
+    </div>
   )
 }
 
